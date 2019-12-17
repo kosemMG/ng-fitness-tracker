a009b5e1a0dc339551bd2a3f42839e5568a4a201
Moshe Gelberg <glbrgm@gmail.com>
2019-12-17 21:12:41 +0200

    AppModule was splitted. AuthModule and TrainingModule were added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { AppRoutingModule } from './app-routing.module';
 
 import { MaterialModule } from "./material.module";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireAuthModule } from 'angularfire2/auth'
-
-import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from "./auth/auth.module";
+import { TrainingModule } from "./training/training.module";
 import { AppComponent } from './app.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
-import { TrainingComponent } from './training/training.component';
-import { CurrentTrainingComponent } from './training/current-training/current-training.component';
-import { NewTrainingComponent } from './training/new-training/new-training.component';
-import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
-import { StopTrainingComponent } from "./training/current-training/stop-training.component";
+import { AngularFireModule } from "angularfire2";
 import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent,
-    LoginComponent,
-    TrainingComponent,
-    CurrentTrainingComponent,
-    NewTrainingComponent,
-    PastTrainingsComponent,
     WelcomeComponent,
     HeaderComponent,
     SidenavListComponent,
-    StopTrainingComponent
   ],
   imports: [
     BrowserModule,
@@ -43,13 +28,10 @@ import { environment } from "../environments/environment";
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    FormsModule,
-    ReactiveFormsModule,
+    AuthModule,
+    TrainingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    AngularFireAuthModule
   ],
   bootstrap: [AppComponent],
-  entryComponents: [StopTrainingComponent]
 })
 export class AppModule { }
