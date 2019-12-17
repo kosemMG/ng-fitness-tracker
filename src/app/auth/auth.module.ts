c3f8d56fac6186c9ced5bba3c92c41859ba812c1
Moshe Gelberg <glbrgm@gmail.com>
2019-12-17 23:12:25 +0200

    Added SharedModule.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from "@angular/common";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { FlexLayoutModule } from "@angular/flex-layout";
 import { AngularFireAuthModule } from "angularfire2/auth";
 
-import { MaterialModule } from "../material.module";
+import { SharedModule } from "../shared/shared.module";
 import { SignupComponent } from "./signup/signup.component";
 import { LoginComponent } from "./login/login.component";
 
@@ -15,13 +13,9 @@ import { LoginComponent } from "./login/login.component";
     LoginComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     ReactiveFormsModule,
-    MaterialModule,
-    FlexLayoutModule,
     AngularFireAuthModule,
-  ],
-  exports: []
+    SharedModule
+  ]
 })
 export class AuthModule {}
--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
-import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AngularFirestoreModule } from "angularfire2/firestore";
 
-import { MaterialModule } from "../material.module";
+import { SharedModule } from "../shared/shared.module";
 import { TrainingComponent } from "./training.component";
 import { CurrentTrainingComponent } from "./current-training/current-training.component";
 import { NewTrainingComponent } from "./new-training/new-training.component";
@@ -21,13 +18,9 @@ import { StopTrainingComponent } from "./current-training/stop-training.componen
     StopTrainingComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
-    MaterialModule,
-    FlexLayoutModule,
+    SharedModule,
     AngularFirestoreModule
   ],
-  exports: [],
   entryComponents: [StopTrainingComponent]
 })
 export class TrainingModule {}
